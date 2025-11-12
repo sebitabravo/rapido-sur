8bf67538d4eca3e99652025c1e0b32a1bf0ac5b3
sebitabravo <sebastianalejandrobravocampos@gmail.com>
2025-11-12 04:37:35 -0300

    feat(backend): agregar enum de prioridad para órdenes de trabajo

--- backend/src/common/enums/index.ts
+++ backend/src/common/enums/index.ts
@@ -5,5 +5,6 @@ export * from "./estado-vehiculo.enum";
 export * from "./rol-usuario.enum";
 export * from "./tipo-orden-trabajo.enum";
 export * from "./estado-orden-trabajo.enum";
+export * from "./prioridad-orden-trabajo.enum";
 export * from "./tipo-intervalo.enum";
 export * from "./tipo-alerta.enum";
